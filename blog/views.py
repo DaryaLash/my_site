93611fe363f8dcbf9898943a5e8e972498006ef6
DaryaLash <darya.lashchevskaya@gmail.com>
2023-01-17 21:22:14 +0300

    added register

---
+++ blog/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+
+
+posts = [
+    {
+        'author': 'Администратор',
+        'title': 'Это мой первый пост',
+        'content': 'Содержимое первого поста',
+        'date_posted': '12 января 2023 года'
+    },
+    {
+        'author': 'Пользователь 1',
+        'title': 'Это мой второй пост',
+        'content': 'Подробное содержание второго поста',
+        'date_posted': '12 января 2023 года'
+    }
+]
+
+
+def home(request):
+    context ={
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title':'О клубе'})
+
